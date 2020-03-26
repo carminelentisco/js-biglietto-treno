ca22714322fc752855244625c1bcf4b62899027a
carmine <carminelentisco@gmail.com>
2020-03-26 13:54:39 +0100

    Logica Base Completata (JS)

--- js/main.js
+++ js/main.js
@@ -11,7 +11,21 @@ console.log('Km Inseriti : ' + km);
 var age = parseInt( prompt('Inserisci la tua età : ') );
 console.log('Anni Inseriti : ' + age);
 
-//var priceKm = 0.25
+var priceKm = 0.21;
+var finalPrice = 0;
+
+// CALCOLO DEL PREZZO 
+
+if (age < 18) {
+    finalPrice += ((( km * priceKm ) * 20) / 100);
+    console.log(finalPrice);
+} else if (age > 65) {
+    finalPrice += ((( km * priceKm ) * 40) / 100);
+    console.log(finalPrice);
+} else {
+    finalPrice += km * priceKm
+    console.log(finalPrice);
+}
 
 
 
