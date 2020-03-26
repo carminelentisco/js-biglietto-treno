6098b30379f6f6f761b61f184d710806a5a51483
carmine <carminelentisco@gmail.com>
2020-03-26 13:39:01 +0100

    Aggiunta File base
    HTML - CSS - JS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// CONTROLLO INSERIMENTO JS
+
+console.log(' -- Il file JavaScript main.js è stato inserito correttamente --');
+
+
+// VARIABILI 
+
+var km = parseInt( prompt('Inserisci i Km da percorrere : ') );
+console.log('Km Inseriti : ' + km);
+
+var age = parseInt( prompt('Inserisci la tua età : ') );
+console.log('Anni Inseriti : ' + age);
+
+//var priceKm = 0.25
+
+
+
+
+
